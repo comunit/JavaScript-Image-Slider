cae9b0767f8aff664e8039e6a7b50b9667f6fbbf
IMRAN RAFIQUE <comunit76@gmail.com>
2017-12-14 02:54:50 +0000

    hello

--- main.js
+++ main.js
@@ -26,6 +26,6 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
